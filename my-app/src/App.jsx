0f215653a7552063a860948a0c9854b5f9d3acb7
unknown <juanaraqueballesteros.jmab@gmail.com>
2023-07-26 20:07:02 -0500

    segundo commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <p>Hola Juan</p>
+        <p>Hola Juan, cambios</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
